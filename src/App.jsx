984b8707475935ddb48585b56291416a5708bfab
Yousef <yousefayman2000500@gmail.com>
2024-04-08 02:08:21 +0200

    storing the user places into local storage

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,13 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const sortedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (sortedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...sortedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
